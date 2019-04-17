08376fff5dca0691376d4b8c0d93eea5503e04cf
Ceiro Kilp <ceiro.kilp@gmail.com>
2019-04-17 23:08:01 +0300

    Init Exchange app

---
+++ Exchange/Controllers/ExchangeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Exchange.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+
+namespace Exchange.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExchangeController : ControllerBase
+    {
+        private IMemoryCache _cache;
+
+        [HttpGet("{from}/{to}/{value}")]
+        public async Task<ConvertedCurrencyModel> Get(string from, string to, double value, IMemoryCache memoryCache)
+        {
+            _cache = memoryCache;
+
+            string url = "https://api.exchangeratesapi.io/latest?base=" + from.ToUpper();
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+
+                string json;
+                using (var content = response.Content)
+                {
+                    json = await content.ReadAsStringAsync();
+                    var result = JsonConvert.DeserializeObject<CurrencysModel>(json);
+
+                    var convertedCurrency = result.Rates.SingleOrDefault(p => p.Key == to.ToUpper()).Value * value;
+
+                    return new ConvertedCurrencyModel(convertedCurrency);
+                }                
+            }
+        }
+    }   
+}
---
+++ Exchange/Models/CurrencysModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exchange.Models
+{
+    public class CurrencysModel
+    {
+        public string Base { get; set; }
+        public Dictionary<string, double> Rates { get; set; }
+    }
+
+    public class ConvertedCurrencyModel
+    {
+        public double ConversionResult { get; set; }
+
+        public ConvertedCurrencyModel(double conversionResult)
+        {
+            this.ConversionResult = conversionResult;
+        }
+    }
+}
